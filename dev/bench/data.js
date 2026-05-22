f76bc1251a4401e2a4a9d8d437a5e36b38379140
github-action-benchmark <github@users.noreply.github.com>
2026-05-22 02:47:49 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 60395cbbf8bfbbf588d00a4700d7547cdccafb64

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779331591229,
+  "lastUpdate": 1779418069630,
   "repoUrl": "https://github.com/gitlayzer/seapack",
   "entries": {
     "Benchmark": [
@@ -3055,6 +3055,78 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/gitlayzer/seapack/commit/60395cbbf8bfbbf588d00a4700d7547cdccafb64"
         },
         "date": 1779331590424,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "deno-2 (arm)",
+            "value": 301397751,
+            "unit": "bytes"
+          },
+          {
+            "name": "deno-2 (x86)",
+            "value": 297271554,
+            "unit": "bytes"
+          },
+          {
+            "name": "go-mod (arm)",
+            "value": 108718598,
+            "unit": "bytes"
+          },
+          {
+            "name": "go-mod (x86)",
+            "value": 86343121,
+            "unit": "bytes"
+          },
+          {
+            "name": "java-gradle (arm)",
+            "value": 547826791,
+            "unit": "bytes"
+          },
+          {
+            "name": "java-gradle (x86)",
+            "value": 531973409,
+            "unit": "bytes"
+          },
+          {
+            "name": "node-npm (arm)",
+            "value": 374370714,
+            "unit": "bytes"
+          },
+          {
+            "name": "node-npm (x86)",
+            "value": 361292424,
+            "unit": "bytes"
+          },
+          {
+            "name": "python-pip (arm)",
+            "value": 427062127,
+            "unit": "bytes"
+          },
+          {
+            "name": "python-pip (x86)",
+            "value": 404282663,
+            "unit": "bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "ⁱⁿ",
+            "username": "gitlayzer",
+            "email": "gduxintian@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "60395cbbf8bfbbf588d00a4700d7547cdccafb64",
+          "message": "chore: mise update 2026.5.12\n\nUpdate mise and dependency locks to 2026.5.12.",
+          "timestamp": "2026-05-20T03:25:08Z",
+          "url": "https://github.com/gitlayzer/seapack/commit/60395cbbf8bfbbf588d00a4700d7547cdccafb64"
+        },
+        "date": 1779418068123,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
